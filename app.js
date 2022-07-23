0c6b52811af934acafca41bb4248a5a4b02e7349
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-24 00:12:54 +0100

    create event in progress

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const form = document.querySelector('#tasks-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Local all event listeners
+loadEventListeners();
+
+// Local all event listeners
+function loadEventListeners(){
+    // Add task event
+    form.addEventListener('submit', addTask);
+};
