fb49d4a75675de094175eeffaaf36c6ffdf46843
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-24 00:07:39 +0100

    Defining the UI variables

---
+++ app.js
@@ -0,0 +1,5 @@
+// Defining UI vars
+const form = document.querySelector('#tasks-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
